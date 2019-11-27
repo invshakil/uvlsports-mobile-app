8662ad80a0d99088fc21ba1a5b23f2c1c17ff875
Syful Shakil <inverse.shakil@gmail.com>
2019-11-27 20:56:41 +0600

    app frontend ready

---
+++ src/app.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- App -->
+  <f7-app>
+    <navbar></navbar>
+    <sidemenu></sidemenu>
+
+    <router-view id="page-wrap"></router-view>
+
+    <a class="footer" href="https://sshakil.com" target="_blank">
+      Developed By Shakil
+      <img
+        src="./assets/copyright.png"
+        style="background: transparent; height: 10px; color: #fff"
+        alt
+      />
+      &nbsp; 2017 - {{ new Date().getFullYear() }}
+    </a>
+  </f7-app>
+</template>
+
+<script>
+// Import Routes
+import navbar from "./components/navbar";
+import sidemenu from "./components/menu";
+
+export default {
+  components: {
+    sidemenu,
+    navbar,
+    // sidebar
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss">
+@import "./styles/main.scss";
+</style>
